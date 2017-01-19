a1316dd13d31e87323a2928900090abaf42e5e06
Angelos Gkountis <agelosbb@gmail.com>
2017-01-19 14:26:47 +0000

    Pathing.

--- GlacierEngine/include/path_component.h
+++ GlacierEngine/include/path_component.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef GLACIER_PATH_COMPONENT_H_
+#define GLACIER_PATH_COMPONENT_H_
+#include "component.h"
+#include <vector>
+
+namespace Glacier
+{
+	class PathComponent : public Component {
+	private:
+		std::vector<Vec4f> m_points;
+
+	public:
+		PathComponent(Object* parent) : Component{ "co_path", parent }
+		{
+		}
+
+		void setup() noexcept override;
+
+		void update(float dt, long time) noexcept override;
+
+		void teardown() noexcept override;
+
+		void add_point(const Vec4f& point) noexcept
+		{
+			m_points.push_back(point);
+		}
+	};
+}
+
+#endif //GLACIER_PATH_COMPONENT_H_
